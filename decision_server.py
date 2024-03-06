2b164281eb0e92dc73b4ad52d76a8ca293f7c5ec
Arad Firouzkouhi <aradfir@gmail.com>
2024-03-07 02:10:13 +0330

    made observation ndarray, moved episode reward count to step func

--- decision_server.py
+++ decision_server.py
@@ -2,6 +2,8 @@
 import time
 import grpc
 from concurrent import futures
+
+import numpy as np
 import observation_decision_pb2
 import observation_decision_pb2_grpc
 import gymnasium as gym
@@ -14,7 +16,9 @@ class CustomGymEnv(gym.Env):
     
     def __init__(self):
         super(CustomGymEnv, self).__init__()
-        self.observation_space = gym.spaces.Tuple((gym.spaces.Discrete(5), gym.spaces.Discrete(5)))
+        # create observation space of the environment, ndarray of 2
+        self.observation_space = gym.spaces.Box(low=-10, high=10, shape=(2,), dtype=np.float32)
+        # self.observation_space = gym.spaces.Tuple((gym.spaces.Discrete(5), gym.spaces.Discrete(5)))
         
         self.action_queue = queue.Queue(1)
         self.observation_queue = queue.Queue(1)
@@ -23,7 +27,7 @@ class CustomGymEnv(gym.Env):
     
     def wait_for_observation_and_return(self):
         observation = self.observation_queue.get(block=True)
-        return observation, {}
+        return self.observation_class_to_ndarray(observation), {}
     
     def reset(self, seed = -1):
         # set selected action as -1, the special reset action
@@ -40,19 +44,22 @@ class CustomGymEnv(gym.Env):
         with self.observation_queue.mutex:
             self.observation_queue.queue.clear()
 
+    def observation_class_to_ndarray(self, observation):
+        return np.array([observation.x, observation.y])
+    
     def do_action(self, action, clear_actions: bool = False):
         if clear_actions:
             self.clear_actions_queue()
         self.action_queue.put(action,block=False)
         
     def get_best_action(self, observation):
-        if observation.x < 2:
+        if observation[0] < 2:
             return 0
-        elif observation.x > 2:
+        elif observation[0] > 2:
             return 2
-        if observation.y < 2:
+        if observation[1] < 2:
             return 1
-        elif observation.y > 2:
+        elif observation[1] > 2:
             return 3
         return 4
     
@@ -61,28 +68,33 @@ class CustomGymEnv(gym.Env):
         self.clear_observation_queue()
         self.do_action(action)
         observation = self.observation_queue.get(block = True)
-        
+       
         # now latest observation is loaded from DecisionServicer
+        converted_observation = self.observation_class_to_ndarray(observation)
         if observation.state == 1:
-            return observation, 1000, True, {}
-        if abs(observation.x) > 10 or abs(observation.x) > 10:
-            return observation, -1000, True, {}
-        
-        manhattan_dist_to_goal = abs(observation.x - 2) + abs(observation.y - 2)
-        reward = -0.5 * manhattan_dist_to_goal
-        return observation, reward, False, {}
+            reward = 1000
+            done = True
+        elif abs(observation.x) > 10 or abs(observation.y) > 10:
+            reward = -1000
+            done = True
+        else:
+            done = False
+            manhattan_dist_to_goal = abs(observation.x - 2) + abs(observation.y - 2)
+            reward = -0.5 * manhattan_dist_to_goal
+        self.episode_reward += reward
+        return converted_observation, reward, done, False, {}
     
 class DecisionServicer(observation_decision_pb2_grpc.DecisionServicer):
     def __init__(self,env):
         super(DecisionServicer, self).__init__()
         self.gym_env: CustomGymEnv = env
     def GetAction(self, request, context):
-        print(f"Received Observation: {request}")
+        # print(f"Received Observation: {request}")
         self.gym_env.clear_actions_queue()
         self.gym_env.observation_queue.put(request, block=False)
         selected_action = self.gym_env.action_queue.get(block = True)           
         action = observation_decision_pb2.Action(action= selected_action)
-        print(f"Sending Action: {action}")
+        # print(f"Sending Action: {action}")
         return action
 
 
@@ -107,15 +119,21 @@ if __name__ == '__main__':
     server_thread = threading.Thread(target=serve, args=(env,))
     server_thread.start()
     # check_env(env)
-    # model = A2C("CnnPolicy", env).learn(total_timesteps=1000)
-    observation, info = env.wait_for_observation_and_return()
+    
+    model = A2C("MlpPolicy", env)
+    # model.learn(total_timesteps=7_000, progress_bar=True)
+    model.load("a2c_custom_gym")
+    print("Training done")
+    # model.save("a2c_custom_gym")
+    input("Press Enter to continue...")
+    # observation, info = env.reset()
+    observation, _ = env.wait_for_observation_and_return()
     while server_thread.is_alive():
         # sample action from the environment
         action = env.get_best_action(observation)
         print(f"Action: {action}")
         # get observation from the environment
-        observation, reward, done, info = env.step(action)
-        env.episode_reward += reward
+        observation, reward, done,truncated, info = env.step(action)
         print(f"Observation: {observation}, Reward: {reward}, Done: {done}, Info: {info}")
         if done:
             observation, info = env.reset()
